54dc6cc6add91e66897095e91d50744893de31f3
Arthur Barrett <abarrett@fas.harvard.edu>
2023-03-21 22:23:09 -0400

    Added lighthouses page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,9 @@ const IndexPage = () => {
     <Layout pageTitle="Home">
       <div>
         <p>Explore {" "}
-          <Link to="/parks">all national parks</Link> or {" "}
-          <Link to="/states">parks in each state</Link>.
+          <Link to="/parks">all national parks</Link>, {" "}
+          <Link to="/states">parks in each state</Link>, {" "}
+          or <Link to="/lighthouses">parks with lighthouses</Link>.
         </p>
         <StaticImage src="../images/acadia.jpg" height={600} alt="Acadia's rocky coastline - NPS / Kristi Rugg"></StaticImage>
       </div>
---
+++ src/pages/lighthouses.js
@@ -0,0 +1,36 @@
+import * as React from "react"
+import { Link, graphql } from 'gatsby';
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const LighthousesPage = ({ data }) => {
+    const parks = data.allParksJson.nodes;
+    return (
+        <Layout pageTitle="Parks with Lighthouses">
+            <ul>
+                {parks.map((park) => {
+                    return (
+                        <li key={park.jsonId}>
+                            <Link to={"/parks/"+park.parkCode}>{park.name}</Link>
+                        </li>)
+                })}
+            </ul>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query {
+        allParksJson(filter: {topics: {elemMatch: {name: {eq: "Lighthouses"}}}}) {
+            nodes {
+                id
+                parkCode
+                name
+            }
+        }
+    }
+`
+
+export const Head = () => <Seo title="Parks with Lighthouses"></Seo>
+
+export default LighthousesPage
